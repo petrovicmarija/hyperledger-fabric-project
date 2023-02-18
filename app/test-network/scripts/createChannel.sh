b91a84154a5ff2b2c98c6e2746dd4a3bf9fa40ff
Marija Petrovic <marijap@protonmail.com>
2023-02-18 02:14:43 +0100

    created channel, added peers to the channel

--- app/test-network/scripts/createChannel.sh
+++ app/test-network/scripts/createChannel.sh
@@ -43,25 +43,26 @@ createChannel() {
 	verifyResult $res "Channel creation failed"
 }
 
-# joinChannel ORG
 joinChannel() {
-  FABRIC_CFG_PATH=$PWD/../config/
-  ORG=$1
-  setGlobals $ORG
+	FABRIC_CFG_PATH=$PWD/../config/
+	ORG=$1
+	PEER_PORT=$2
+	PEERS=$3
+	setGlobalsPeers $ORG $PEER_PORT
 	local rc=1
 	local COUNTER=1
 	## Sometimes Join takes time, hence retry
 	while [ $rc -ne 0 -a $COUNTER -lt $MAX_RETRY ] ; do
-    sleep $DELAY
-    set -x
-    peer channel join -b $BLOCKFILE >&log.txt
-    res=$?
-    { set +x; } 2>/dev/null
-		let rc=$res
-		COUNTER=$(expr $COUNTER + 1)
+		sleep $DELAY
+		set -x
+		peer channel join -b $BLOCKFILE >&log.txt
+		res=$?
+		{ set +x; } 2>/dev/null
+			let rc=$res
+			COUNTER=$(expr $COUNTER + 1)
 	done
 	cat log.txt
-	verifyResult $res "After $MAX_RETRY attempts, peer0.org${ORG} has failed to join channel '$CHANNEL_NAME' "
+	verifyResult $res "After $MAX_RETRY attempts, peer${PEERS}.org${ORG} has failed to join channel '$CHANNEL_NAME' "
 }
 
 setAnchorPeer() {
@@ -83,11 +84,49 @@ infoln "Creating channel ${CHANNEL_NAME}"
 createChannel
 successln "Channel '$CHANNEL_NAME' created"
 
-## Join all the peers to the channel
-infoln "Joining org1 peer to the channel..."
-joinChannel 1
-infoln "Joining org2 peer to the channel..."
-joinChannel 2
+cnt=0
+current_port=7051
+infoln "Joining org1 peers to the channel..."
+while [ $cnt -lt 4 ]
+do
+	infoln "Joining org1 peer${cnt} at port ${current_port} to the channel..."
+	joinChannel 1 $current_port $cnt
+	current_port=$(($current_port+100))
+	cnt=$(($cnt+1))
+done
+
+cnt=0
+current_port=9051
+infoln "Joining org2 peers to the channel..."
+while [ $cnt -lt 4 ]
+do
+	infoln "Joining org2 peer${cnt} at port ${current_port} to the channel..."
+	joinChannel 2 $current_port $cnt
+	current_port=$(($current_port+100))
+	cnt=$(($cnt+1))
+done
+
+cnt=0
+current_port=10051
+infoln "Joining org3 peers to the channel..."
+while [ $cnt -lt 4 ]
+do
+	infoln "Joining org3 peer${cnt} at port ${current_port} to the channel..."
+	joinChannel 3 $current_port $cnt
+	current_port=$(($current_port+100))
+	cnt=$(($cnt+1))
+done
+
+cnt=0
+current_port=11051
+infoln "Joining org4 peers to the channel..."
+while [ $cnt -lt 4 ]
+do
+	infoln "Joining org4 peer${cnt} at port ${current_port} to the channel..."
+	joinChannel 4 $current_port $cnt
+	current_port=$(($current_port+100))
+	cnt=$(($cnt+1))
+done
 
 ## Set the anchor peers for each org in the channel
 infoln "Setting anchor peer for org1..."
